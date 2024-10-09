d85f2119f2236bb5210c092c3e22e496653553a7
Rebecca Bretzinger <rebeccaindy@gmail.com>
2024-10-09 12:24:53 -0600

    update

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -34,62 +34,20 @@ const AboutSection = () => {
         </h1>
 
         <div className="flex flex-col space-y-10 items-stretch justify-center align-top sm:mx-12 md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
-          <div className="relative md:w-1/2 ">
-            {/* <h1 className="text-center sm:text-left text-2xl font-bold mb-6 md:text-left">
-              Get to know me!
-            </h1> */}
-            {/* <p>
-              Hi, my name is Becca and I am a{" "}
-              <span className="font-bold">{"self-motivated"}</span>,
-              <span className="font-bold">{" positive"}</span>, and
-              <span className="font-bold">{" supportive"}</span> Frontend Developer
-              based near Salt Lake City, Utah.
-            </p>
-            <br /> */}
+        <div className="image-col">
+          <Image className="rounded-full" src="/rebecca-bretzinger-hubspot-cms-developer2.jpg" alt="Rebecca Bretzinger" width={340} height={340} />
+        </div>
+
+<div className="text-col">
             <p>
             With over 19 years at New Era Technology, I’ve had the privilege of working with some of the world’s most recognizable brands, including Eli Lilly, Fidelity, Sallie Mae, Starbucks, and the Indianapolis Motor Speedway. My role often involved leading the technical implementation of solutions, including web development, complex email automation, responsive email templates, and CRM integrations. I’ve also onboarded and overseen other developers, refining workflows and documenting processes to ensure seamless collaboration across teams.           <br />
             </p>
             <p>
             I live in Utah with my husband, surrounded by the beautiful mountains that are perfect for our road trips and outdoor adventures with our dog. This year, we proudly watched our son embark on a new journey with the Navy after graduating from high school. We couldn't be prouder. When I'm not exploring the outdoors or studying for a new certification, I can be found at concerts enjoying the music and the community, listening to podcasts, or binge watching a TV series.</p>
-            <br />
-            {/* <br />
-            <p>
-              I believe that you should{" "}
-              <span className="font-bold text-primary">
-                never stop growing
-              </span>{" "}
-              and that&#39;s what I strive to do, I have a passion for
-              technology and a desire to always push the limits of what is
-              possible. I am excited to see where my career takes me and am
-              always open to new opportunities. 🙂
-            </p> */}
           </div>
           <div className="relative text-left md:w-1/2 md:text-left">
 
-          <div className="image-col">
-          <Image className="rounded-full" src="/rebecca-bretzinger-hubspot-cms-developer2.jpg" alt="Rebecca Bretzinger" width={340} height={340} />
-        </div>
-
-            {/* <h1 className="text-2xl font-bold mb-6">My Skills</h1>
-            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
-              {skills.map((item, idx) => {
-                return (
-                  <p
-                    key={idx}
-                    className="bg-gray-50 border border-slate-400 px-2 py-1 m-1 text-gray-600 font-medium"
-                  >
-                    {item.skill}
-                  </p>
-                )
-              })}
-            </div> */}
-            {/* <img
-              src="/mountain1.png"
-              alt=""
-              width={420}
-              height={420}
-              className="mx-auto sm:mt-8 md:mt-0 md:float-right bottom-0 right-0 z-0"
-            /> */}
+          
           </div>
         </div>
       </div>
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,54 +3,53 @@ import React from "react"
 import Image from "next/image"
 import { Link } from "react-scroll/modules"
 
-
 const HeroSection = () => {
   return (
     <section id="home">
-      {/* Wrapper for the hero section */}
-      <div className="col-wrapper">
-        
-        {/* Column for the image */}
-        <div className="image-col">
-        <div className="relative w-64 h-64">
-
-          <Image className="rounded-full" src="/Rebecca7-square.png" alt="Rebecca Bretzinger" width={370}
-    height={370}  />
-          </div>
-        </div>
-
-        {/* Column for the text and button */}
+      <div className="hero-col-wrapper">
         <div className="text-col">
           <h1>Hello, I&#39;m Rebecca!</h1>
           <p>
-          With 19 years of experience as a web developer and marketing technology specialist, I’ve worked across a wide variety of roles for notable companies. This year, I earned certifications as a Salesforce Marketing Cloud Email Specialist and Developer. Scroll down to learn more about me and explore key web development and SFMC projects I’ve worked on.
-          
-          </p>          <div className="links-wrapper">
-  <Link
-    to="about"
-    className="link-with-arrow"
-    smooth={true}
-    offset={-84}  // Adjust the offset to match your sticky header height
-    duration={500}
-  >
-    <h3>More About Me</h3>
-    <i className="fas fa-arrow-right"></i>
-  </Link>
+            With 19 years of experience as a web developer and marketing
+            technology specialist, I’ve worked across a wide variety of roles
+            for notable companies. This year, I earned certifications as a
+            Salesforce Marketing Cloud Email Specialist and Developer. Scroll
+            down to learn more about me and explore key web development and SFMC
+            projects I’ve worked on.
+          </p>
+          <div className="links-wrapper">
+            <Link
+              to="about"
+              className="link-with-arrow"
+              smooth={true}
+              offset={-84} // Adjust the offset to match your sticky header height
+              duration={500}>
+              <h3>More About Me</h3>
+              <i className="fas fa-arrow-right"></i>
+            </Link>
 
-  <Link
-    to="projects"
-    className="link-with-arrow"
-    smooth={true}
-    offset={-84}  // Same offset for consistent behavior
-    duration={500}
-  >
-    <h3>View My Projects</h3>
-    <i className="fas fa-arrow-right"></i>
-  </Link>
-</div>
-
-        </div>
-      </div>
+            <Link
+              to="projects"
+              className="link-with-arrow"
+              smooth={true}
+              offset={-84} // Same offset for consistent behavior
+              duration={500}>
+              <h3>View My Projects</h3>
+              <i className="fas fa-arrow-right"></i>
+            </Link>
+          </div>
+          </div>
+          <div className="image-col">
+            <div className="image-wrapper">
+              <Image
+                src="/Rebecca7-square.png"
+                alt="Rebecca Bretzinger"
+                width={370}
+                height={370}
+              />
+          </div>
+          </div>
+          </div>
     </section>
   )
 }
